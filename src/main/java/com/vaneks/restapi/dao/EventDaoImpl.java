f388d2f15c5dfc7d37734e8d3f10798b3514f631
Ivan <35092751+vaneks@users.noreply.github.com>
2021-06-21 11:27:13 +0800

    Update EventDaoImpl.java

--- src/main/java/com/vaneks/restapi/dao/EventDaoImpl.java
+++ src/main/java/com/vaneks/restapi/dao/EventDaoImpl.java
@@ -1,10 +1,21 @@
 package com.vaneks.restapi.dao;
 
 import com.vaneks.restapi.model.Event;
+import org.hibernate.query.Query;
+
+import java.util.List;
 
 public class EventDaoImpl extends AbstractDao<Event> {
     public EventDaoImpl(String className, Class<Event> eventClass) {
         super(className, eventClass);
-
     }
+
+        public List<Event> getUserAll(long user_id) {
+            session.beginTransaction();
+            Query query = session.createQuery("FROM Event E WHERE E.user.id =: user_id");
+            query.setParameter("user_id", user_id);
+            List<Event> entity = query.list();
+            session.getTransaction().commit();
+            return entity;
+        }
 }
